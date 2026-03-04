9c5ce7408ba144be46c33f525261f709ca282458
Mauro Ramirez <mauroamaya58@gmail.com>
2026-03-04 13:37:04 -0300

    chore: Clean up massive accumulation of debug logs, test scripts, and temporary JSON dumps from root and tmp-debug

--- test-imports.js
+++
@@ -1,8 +0,0 @@
-import * as utils from './src/layer-a-static/extractors/data-flow/utils/ts-ast-utils.js';
-console.log('Exports found:', Object.keys(utils));
-if (utils.getAssignmentTarget) {
-    console.log('✅ getAssignmentTarget is exported');
-} else {
-    console.error('❌ getAssignmentTarget is MISSING');
-    process.exit(1);
-}
